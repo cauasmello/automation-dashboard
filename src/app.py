a8df6a68f2b341bcb28a52bcaccef32bfbb4b3c7
Cauã Stulpen Mello <caua.mello@t2mlab.com>
2026-03-11 10:48:14 -0300

    Lancamentos

--- src/app.py
+++ src/app.py
@@ -11,6 +11,7 @@ pg = st.navigation(
     [
         st.Page("pages/dashboard.py", title="Dashboard Operacional", icon="📊"),
         st.Page("pages/analise_dados.py", title="Análise de Dados", icon="📈"),
+        st.Page("pages/cadastro_lancamentos.py", title="Lançamento de Dados", icon="📈")
     ]
 )
 
